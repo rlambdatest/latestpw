bfbb8033e0115715fe166016d65e3a9bfffd65aa
rlambdatest <132660047+rlambdatest@users.noreply.github.com>
2023-06-05 00:54:32 +0530

    Update sample.test.js

--- playwright-jest-js/sample.test.js
+++ playwright-jest-js/sample.test.js
@@ -17,14 +17,14 @@ describe("Bing Search", () => {
     await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({action: 'setTestStatus',arguments: {status: 'passed',remark: 'Test assertion passed'}})}`);
   })
 
-  it('title should contain LambdaTest- Bing Search', async () => {
-    await page.type('[id="sb_form_q"]', "LambdaTest");
-    await page.waitForTimeout(1000)
-    await page.keyboard.press("Enter");
-    await page.waitForSelector('[class=" b_active"]')
-    const title = await page.title()
-    console.log('Page title:: ', title)
-    expect(title).toBe('LambdaTest - Search')
-    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({action: 'setTestStatus',arguments: {status: 'passed',remark: 'Test assertion passed'}})}`);
-  })
+//   it('title should contain LambdaTest- Bing Search', async () => {
+//     await page.type('[id="sb_form_q"]', "LambdaTest");
+//     await page.waitForTimeout(1000)
+//     await page.keyboard.press("Enter");
+//     await page.waitForSelector('[class=" b_active"]')
+//     const title = await page.title()
+//     console.log('Page title:: ', title)
+//     expect(title).toBe('LambdaTest - Search')
+//     await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({action: 'setTestStatus',arguments: {status: 'passed',remark: 'Test assertion passed'}})}`);
+//   })
 })
