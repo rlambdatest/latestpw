1afb685f73245a02c7285fa4608737aab1135548
rlambdatest <132660047+rlambdatest@users.noreply.github.com>
2023-06-05 00:53:16 +0530

    Update jest-playwright.config.js

--- playwright-jest-js/jest-playwright.config.js
+++ playwright-jest-js/jest-playwright.config.js
@@ -1,24 +1,24 @@
 const cp = require('child_process');
 const playwrightClientVersion = cp.execSync('npx playwright --version').toString().trim().split(' ')[1];
 
-const caps_chromium = {
-  'browserName': 'pw-chromium', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
-  'browserVersion': 'latest',
-  'LT:Options': {
-    'platform': 'Windows 10',
-    'build': 'Playwright-jest sample Build',
-    'name': 'Playwright-jest test on Chromium',
-    'user': "rakeshslambdatest",
-    'accessKey':"MHu1nfiZVVPSteHsFVqybmvsxePoFJq0j8htKDk7uHcRwPgLGg",
-    'network': true,
-    'video': true,
-    'console': true,
-    'tunnel': false, // Add tunnel configuration if testing locally hosted webpage
-    // 'tunnelName': '', // Optional
-    // 'geoLocation': '', // country code can be fetched from https://www.lambdatest.com/capabilities-generator/
-    'playwrightClientVersion': playwrightClientVersion
-  }
-};
+// const caps_chromium = {
+//   'browserName': 'pw-chromium', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+//   'browserVersion': 'latest',
+//   'LT:Options': {
+//     'platform': 'Windows 10',
+//     'build': 'Playwright-jest sample Build',
+//     'name': 'Playwright-jest test on Chromium',
+//     'user': "rakeshslambdatest",
+//     'accessKey':"MHu1nfiZVVPSteHsFVqybmvsxePoFJq0j8htKDk7uHcRwPgLGg",
+//     'network': true,
+//     'video': true,
+//     'console': true,
+//     'tunnel': false, // Add tunnel configuration if testing locally hosted webpage
+//     // 'tunnelName': '', // Optional
+//     // 'geoLocation': '', // country code can be fetched from https://www.lambdatest.com/capabilities-generator/
+//     'playwrightClientVersion': playwrightClientVersion
+//   }
+// };
 
 const caps_firefox = {
   'browserName': 'pw-firefox', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
@@ -39,35 +39,35 @@ const caps_firefox = {
   }
 };
 
-const caps_webkit = {
-  'browserName': 'pw-webkit', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
-  'browserVersion': 'latest',
-  'LT:Options': {
-    'platform': 'Windows 10',
-    'build': 'Playwright-jest sample Build',
-    'name': 'Playwright-jest test on Webkit',
-    'user': "rakeshslambdatest",
-    'accessKey': "MHu1nfiZVVPSteHsFVqybmvsxePoFJq0j8htKDk7uHcRwPgLGg",
-    'network': true,
-    'video': true,
-    'console': true,
-    'tunnel': false, // Add tunnel configuration if testing locally hosted webpage
-    // 'tunnelName': '', // Optional
-    // 'geoLocation': '', // country code can be fetched from https://www.lambdatest.com/capabilities-generator/
-  }
-};
+// const caps_webkit = {
+//   'browserName': 'pw-webkit', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+//   'browserVersion': 'latest',
+//   'LT:Options': {
+//     'platform': 'Windows 10',
+//     'build': 'Playwright-jest sample Build',
+//     'name': 'Playwright-jest test on Webkit',
+//     'user': "rakeshslambdatest",
+//     'accessKey': "MHu1nfiZVVPSteHsFVqybmvsxePoFJq0j8htKDk7uHcRwPgLGg",
+//     'network': true,
+//     'video': true,
+//     'console': true,
+//     'tunnel': false, // Add tunnel configuration if testing locally hosted webpage
+//     // 'tunnelName': '', // Optional
+//     // 'geoLocation': '', // country code can be fetched from https://www.lambdatest.com/capabilities-generator/
+//   }
+// };
 
 module.exports = {
     connectOptions: {      
-        chromium: {
-          wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(caps_chromium))}`
-        },
+//         chromium: {
+//           wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(caps_chromium))}`
+//         },
         firefox: {
           wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(caps_firefox))}`
         },
-        webkit: {
-            wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(caps_webkit))}`
-        }
+//         webkit: {
+//             wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(caps_webkit))}`
+//         }
       },
       browsers: ['chromium', 'firefox', 'webkit'],
 }
